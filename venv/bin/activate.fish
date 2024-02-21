a05bc7a7a1a39bc202a76d2874546cf08461f178
vintze <vintzeanu@gmail.com>
2024-02-21 18:03:54 +0200

    requirements file

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/vintze/python/flashcards_app/venv"
+set -gx VIRTUAL_ENV "/Users/vintze/python/code/flashcards_app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
