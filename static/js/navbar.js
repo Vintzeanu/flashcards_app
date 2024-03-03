d789ddabf43e16bd645c036487b19cd677923c7d
vintze <vintzeanu@gmail.com>
2024-03-03 09:35:57 +0200

    Acasă button fixed, js script have separate file

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,8 +1,8 @@
 function myFunction() {
     var x = document.getElementById("nav");
     if (x.className === "navbar") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "navbar";
+        x.className = "navbar";
     }
-  }
+}
